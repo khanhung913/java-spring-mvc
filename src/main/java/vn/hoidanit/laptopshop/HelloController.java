43b50c6dd25c00f6917b61b45433e38528514c7c
NGUYEN KHANH HUNG <khanhung913@gmail.com>
2024-08-10 12:14:59 +0900

    0810

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,6 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World from Spring Boot!";
+        return "Hello World from Spring !";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "User";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Admin";
     }
 }
